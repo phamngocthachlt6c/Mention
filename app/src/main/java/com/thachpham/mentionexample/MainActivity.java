ae260482454ef15b5ac441ac5a008cbcf8bc5f41
Thach Pham <thachpn2>
2019-11-06 17:05:24 +0700

    init

---
+++ app/src/main/java/com/thachpham/mentionexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.thachpham.mentionexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
